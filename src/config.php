91582403ec2e07e241d3896dd3b00d7b0305b78c
Lasitha64 <68686464+Lasitha64@users.noreply.github.com>
2020-10-20 18:24:09 +0530

    Database added

---
+++ src/config.php
@@ -0,0 +1,18 @@
+<?php 
+$servername="localhost";
+$username="root";
+$password="";
+$db = "commentdb";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if($conn -> connect_error){
+	die("Connection Failed :".$conn->connect_error);
+}
+else{
+	echo "Connected Succefully";
+}
+
+
+
+?>
